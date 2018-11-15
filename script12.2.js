3778fba3c8985965ce4d0217c1b9860610b057e0
Arteom333 <porchiesku@bk.ru>
2018-11-15 15:29:38 +0200

    APT

---
+++ script12.2.js
@@ -0,0 +1,16 @@
+// function onClickHandler(event) {
+//  var Calculate = document.getElementById("")
+//  document.getElementById("click-me").addEventListener('click',onClickHandler)
+
+// }
+
+// document.getElementById("click-me").addEventListener('click',onClickHandler)
+
+var x = prompt("Введите х", 'x=');
+var n = prompt("Введите n", 'n=');
+function pow(x, n) {
+   return Math.pow(x, n);
+   
+}
+alert(pow(x,n));
+
